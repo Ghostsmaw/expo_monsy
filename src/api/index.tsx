eb46ff1375b14103fed72a0023cfa7957773b26e
Dang Quang Minh <minh@grooovyware.jp>
2023-03-13 02:00:25 +0700

    fix

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,17 +1,19 @@
-import { getToken } from '../utils/store/Store';
+import { getToken } from "../utils/store/Store";
 
-/*** MUST-CONFIG ***/
-export const SERVER_ADDRESS = 'http://localhost:3000';
+/*** MUST-CONFIG (DONE) ***/
+// AWS SERVER:
+// https://eu-north-1.console.aws.amazon.com/ec2/v2/home?region=eu-north-1#InstanceDetails:instanceId=i-0a2b5445f2c54c14f
+export const SERVER_ADDRESS = "http://16.170.248.45:3000";
 
-export const BEARER = 'Bearer';
-export const API_ENDPOINT = SERVER_ADDRESS + '/api/';
+export const BEARER = "Bearer";
+export const API_ENDPOINT = SERVER_ADDRESS + "/api/";
 
 const getParam = (method: string, data: any, token = null) => {
   return {
     method: method,
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
       Authorization: `${BEARER} ${token}`,
     },
     body: data && JSON.stringify(data),
@@ -22,7 +24,7 @@ const getParamFile = (method: string, data: any, token = null) => {
   return {
     method: method,
     headers: {
-      'Content-Type': 'multipart/form-data',
+      "Content-Type": "multipart/form-data",
       Authorization: `${BEARER} ${token}`,
     },
     body: data,
@@ -36,15 +38,15 @@ export async function request(
   endpoint: string,
   method: string,
   body: any,
-  navigate_token: any,
+  navigate_token: any
 ) {
   try {
     const token = navigate_token || (await getToken());
     let response = await fetch(
       API_ENDPOINT + endpoint,
-      getParam(method, body, token),
+      getParam(method, body, token)
     );
-    console.log('REQUEST', method + ' : ' + API_ENDPOINT + endpoint);
+    console.log("REQUEST", method + " : " + API_ENDPOINT + endpoint);
     let data = await response.json();
     return handleError(data);
   } catch (error) {
@@ -59,13 +61,13 @@ export async function requestFile(
   endpoint: string,
   method: string,
   body: any,
-  navigate_token: any,
+  navigate_token: any
 ) {
   try {
     const token = navigate_token || (await getToken());
     let response = await fetch(
       API_ENDPOINT + endpoint,
-      getParamFile(method, body, token),
+      getParamFile(method, body, token)
     );
     let data = await response.json();
     return handleError(data);
@@ -91,98 +93,98 @@ const handleError = (response: any) => {
 
 // Login
 export const apiSignIn = (body: any) => {
-  return request('user/login', 'POST', body, null);
+  return request("user/login", "POST", body, null);
 };
 
 // Get user by token
 export const apiGetUserByToken = () => {
-  return request('user/get/token', 'GET', null, null);
+  return request("user/get/token", "GET", null, null);
 };
 
 // create wallet
 export const apiCreateWallet = (body: any) => {
-  return request('wallet/create', 'POST', body, null);
+  return request("wallet/create", "POST", body, null);
 };
 
 // get list wallet
 export const apiGetWallets = ({ page = 0, limit = 100 }) => {
-  return request(`wallet/list/${page}/${limit}`, 'GET', null, null);
+  return request(`wallet/list/${page}/${limit}`, "GET", null, null);
 };
 //update wallet
 /*
  */
 export const apiUpdateWallet = (body: any) => {
-  return request('wallet/update', 'PUT', body, null);
+  return request("wallet/update", "PUT", body, null);
 };
 //delete wallet
 /*
  */
 export const apiDeleteWallet = (body: any) => {
-  return request('wallet/delete', 'DELETE', body, null);
+  return request("wallet/delete", "DELETE", body, null);
 };
 // create transaction
 export const apiCreateTransaction = (body: any) => {
-  return request('transaction/create', 'POST', body, null);
+  return request("transaction/create", "POST", body, null);
 };
 
 // get list transaction
 export const apiGetTransactions = ({ page = 0, limit = 100 }) => {
-  return request(`transaction/list/${page}/${limit}`, 'GET', null, null);
+  return request(`transaction/list/${page}/${limit}`, "GET", null, null);
 };
 
 // get dashboard data (wallets & latest transaction)
 export const apiGetDashboardInformation = () => {
-  return request('user/dashboard', 'GET', null, null);
+  return request("user/dashboard", "GET", null, null);
 };
 
 // get list transaction by month
 export const apiGetTransactionByMonth = ({
-  year = '2020',
-  month = '01',
+  year = "2020",
+  month = "01",
   page = 0,
   limit = 100,
 }) => {
   return request(
     `transaction/list/${year}/${month}/${page}/${limit}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
 
 export const apiGetTransactionWalletByMonth = ({
   walletId = 1,
-  year = '2021',
-  month = '02',
+  year = "2021",
+  month = "02",
   page = 0,
   limit = 100,
 }) => {
   return request(
     `wallet/get/${walletId}/transactions/${year}/${month}/${page}/${limit}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
 
 //Wallet git list transaction by Date
 export const apiGetTransactionWalletByDate = (body: any) => {
-  return request(`wallet/get_transactions_by_date`, 'POST', body, null);
+  return request(`wallet/get_transactions_by_date`, "POST", body, null);
 };
 
 // get list transaction by date range
 export const getTransactionWalletByDateRange = ({
   walletId = 1,
-  start = '2021-01-01',
-  end = '2021-12-31',
+  start = "2021-01-01",
+  end = "2021-12-31",
   page = 0,
   limit = 100,
 }) => {
   return request(
     `wallet/get/${walletId}/transactions/${start}}/${end}}/${page}/${limit}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
 
@@ -190,50 +192,50 @@ export const getTransactionWalletByDateRange = ({
 /*
  */
 export const apiUpdateTransaction = (body: any) => {
-  return request('transaction/update', 'PUT', body, null);
+  return request("transaction/update", "PUT", body, null);
 };
 //delete wallet
 /*
  */
 export const apiDeleteTransaction = (body: any) => {
-  return request('transaction/delete', 'DELETE', body, null);
+  return request("transaction/delete", "DELETE", body, null);
 };
 //delete all wallet
 /*
  */
 export const apiDeleteAllTransaction = (body: any) => {
-  return request('transaction/delete/all', 'DELETE', body, null);
+  return request("transaction/delete/all", "DELETE", body, null);
 };
 
 //update currency
 /*
  */
 export const apiUpdateCurrency = (body: any) => {
-  return request('user/currency/update', 'PUT', body, null);
+  return request("user/currency/update", "PUT", body, null);
 };
 
 /*
  *Chart get transaction all wallet
  */
 export const apiChartGetTransactionAllWallet = ({
-  year = '2020',
-  month = '01',
+  year = "2020",
+  month = "01",
 }) => {
-  return request(`transaction/chart/${year}/${month}`, 'GET', null, null);
+  return request(`transaction/chart/${year}/${month}`, "GET", null, null);
 };
 
 /*
  *Chart get transaction year all wallet
  */
-export const apiChartGetTransactionYearAllWallet = ({ year = '2020' }) => {
-  return request(`transaction/chart/${year}`, 'GET', null, null);
+export const apiChartGetTransactionYearAllWallet = ({ year = "2020" }) => {
+  return request(`transaction/chart/${year}`, "GET", null, null);
 };
 
 /*
  *Chart get all transaction all wallet
  */
 export const apiChartGetAllTransactionAllWallet = () => {
-  return request(`transaction/chart/all`, 'GET', null, null);
+  return request(`transaction/chart/all`, "GET", null, null);
 };
 
 /*
@@ -241,14 +243,14 @@ export const apiChartGetAllTransactionAllWallet = () => {
  */
 export const apiChartGetTransactionByWallet = ({
   walletId = 1,
-  year = '2020',
-  month = '01',
+  year = "2020",
+  month = "01",
 }) => {
   return request(
     `wallet/chart/${walletId}/transaction/${year}/${month}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
 
@@ -257,13 +259,13 @@ export const apiChartGetTransactionByWallet = ({
  */
 export const apiChartGetTransactionYearByWallet = ({
   walletId = 1,
-  year = '2020',
+  year = "2020",
 }) => {
   return request(
     `wallet/chart/${walletId}/transaction/${year}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
 
@@ -271,18 +273,18 @@ export const apiChartGetTransactionYearByWallet = ({
  *Chart get all transaction wallet
  */
 export const apiChartGetAllTransactionByWallet = ({ walletId = 1 }) => {
-  return request(`wallet/chart/${walletId}/transaction`, 'GET', null, null);
+  return request(`wallet/chart/${walletId}/transaction`, "GET", null, null);
 };
 
 /*
  *Chart get transaction all wallet
  */
-export const apiDetailChartGetAllWallet = ({ year = '2020', month = '01' }) => {
+export const apiDetailChartGetAllWallet = ({ year = "2020", month = "01" }) => {
   return request(
     `transaction/detail_chart/${year}/${month}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
 
@@ -291,21 +293,21 @@ export const apiDetailChartGetAllWallet = ({ year = '2020', month = '01' }) => {
  */
 export const apiDetailChartGetOneWallet = ({
   walletId = -1,
-  year = '2020',
-  month = '01',
+  year = "2020",
+  month = "01",
 }) => {
   if (walletId == -1) {
     return request(
       `transaction/detail_chart/${year}/${month}`,
-      'GET',
+      "GET",
       null,
-      null,
+      null
     );
   }
   return request(
     `transaction/detail_chart/${walletId}/${year}/${month}`,
-    'GET',
+    "GET",
     null,
-    null,
+    null
   );
 };
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,10 @@
-/*** MUST-CONFIG ***/
+/*** MUST-CONFIG (DONE) ***/
 export const firebaseConfig = {
-  measurementId: '',
-  apiKey: 'AIzaSyCCTzz1RXga3itwYv-YcW3zEmGLwe1mBL8',
-  authDomain: 'monsy-demo-application.firebaseapp.com',
-  projectId: 'monsy-demo-application',
-  storageBucket: 'monsy-demo-application.appspot.com',
-  messagingSenderId: '1058017311089',
-  appId: '1:1058017311089:web:510cd36919bb68739c5ed0',
+  measurementId: "G-0R28YR8VKN",
+  apiKey: "AIzaSyApnX3B25-osqrmjNhYPcrvw5gO6v16tQk",
+  authDomain: "kollo-app-8295a.firebaseapp.com",
+  projectId: "kollo-app-8295a",
+  storageBucket: "kollo-app-8295a.appspot.com",
+  messagingSenderId: "244032969090",
+  appId: "1:244032969090:web:06d6cf99e3ca5ec1f4579b",
 };
